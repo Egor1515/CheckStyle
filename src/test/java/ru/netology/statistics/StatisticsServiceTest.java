b9eb4d4fb8dd6a7041413f6d63f08d35afb42fd0
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-02-09 17:45:38 +1000

    Added CI

---
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.statistics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findAverage() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 7;
+
+        long actual = service.findAverage(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
