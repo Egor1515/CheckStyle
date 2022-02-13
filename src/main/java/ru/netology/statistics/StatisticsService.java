5adcde18b21df0e628a43f50cdd39f61485e32a9
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-02-13 19:05:13 +1000

    Fix plugin

--- src/main/java/ru/netology/statistics/StatisticsService.java
+++ src/main/java/ru/netology/statistics/StatisticsService.java
@@ -2,7 +2,7 @@ package ru.netology.statistics;
 
 public class StatisticsService {
 
-    public long findMax(long[] incomes) {
+     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
